f6432a3b9aa42a0c0320bc69e2986ae0d6032be5
LOK <bax.without.tongue@gmail.com>
2026-04-02 15:42:43 +0800

    Add a file called Product.py to create a product class including all details about product

--- Task1/main.py
+++ Task1/main.py
@@ -1,5 +1,8 @@
+# This is the main entry point for the Smart Inventory Management System
+
+# Import the SmartInventorySystem class from inventory.py
 from inventory import SmartInventorySystem
 
-if __name__ == "main":
-    system = SmartInventorySystem()
-    system.start_system()
+if __name__ == "__main__":
+    system = SmartInventorySystem()     # Create an inventory management system instance
+    system.start_system()               # Run the system
---
+++ Task1/product.py
@@ -0,0 +1,22 @@
+# Define the Product class
+
+Class Product:
+    def__init__(self, data):
+        pass
+        # product id
+        # name
+        # price
+        # supplier
+        # stock level
+        # safety stock
+        # lead time
+        # ordering cost
+        # holding cost
+        # quantity delivered
+        # quantity returned
+    
+    def supplier_kpi_score(self):
+        pass
+    
+    def reorder_point(self):
+        pass
