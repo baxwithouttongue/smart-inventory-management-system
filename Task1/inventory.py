9c2fde04809878aec6e01249cc260993972b1d29
LOK <bax.without.tongue@gmail.com>
2026-04-05 00:36:29 +0800

    Update exployee_role.csv, supplier.py and inventory.py

--- Task1/inventory.py
+++ Task1/inventory.py
@@ -15,14 +15,14 @@ class SmartInventorySystem:
         self.prepare_files ()   # Load data from employee, product and supplier files
 
     def prepare_files(self):
-       
-        employee_roles = pd.read_csv('csv/employee_role.csv')       # Load employee data from employee_role.csv
+        employee_roles = pd.read_csv('csv/employee_role.csv')      # Load employee data from employee_role.csv
         
-        for index, row in employee_roles.iterrows():
-            role = row['role'].strip()          # Get the role text and clean spaces at the beginning or the end of the text.
+        for index, row in employee_roles.iterrows():                # Create a loop to read the employee_role.csv line by line
+            role = row['role'].strip()                              # Get the role text and clean spaces at the beginning or the end of the text.
             employee_name = row [employee_name].strip().lower()     # Get the employee name and clean spaces at the beginning or the end of the text. Convert the text to the lower case.
-            pw = row['password'].strip()        # Get the password data and clean spaces at the beginning or the end of the text.
+            pw = row['password'].strip()                            # Get the password data and clean spaces at the beginning or the end of the text.
             
+            # Check the user employee role
             if role == 'Manager':
                 self.users[employee_name] = Manager(employee_name, pw, role)
             elif role == 'Supervisior':
@@ -31,8 +31,40 @@ class SmartInventorySystem:
                 self.users[employee_name] = Assistant(employee_name, pw, role)
 
         # Load product data from product.csv
-            
+        product_list = pd.read.csv('csv/product.csv')       # Read product data from employee_role.csv
+        
+        for index, row in product_list.iterrows():          # Create a loop to read the product.csv line by line
+            self.product = product(row)
+        
 
         # Load supplier data from supplier.csv
 
-        pass
+        suppliers_info = pd.read_csv('supplier.csv')         # Create a loop to read supplier.csv line by line
+        
+        for index, row in suppliers_info.iterrows():
+            self.suppliers = supplier(
+                row['supplier_name'].strip(),
+                row['contact_person'].strip(),
+                row['email'].strip(),
+                row['phone'].strip()
+            )
+
+    # User log in authentication
+    # if name/password/ = username/password
+
+    # System starts
+    # Assign each employee identity and their level of access
+
+    # Ask user which product to view or view all
+    # Find the product by product ID
+
+    # View and show the product detail and print the detail
+
+    # Add a new product detail
+
+    # Create a dictionary for the new product detail
+
+    # Update product list
+
+
+
--- Task1/supplier.py
+++ Task1/supplier.py
@@ -1,20 +1,27 @@
-# This is the blueprint for the SupplierDirectory class.
+# This is the blueprint for the Supplier and SupplierDirectory class.
 # It provides the objects and methods used by the inventory system.
 
-class SupplierDirectory:                # Define a class called SupplierDirectory
+class Supplier:                                                         # Define a class called Supplier
+    def __init__(self, data):                                           # Create a constructor method and create supplier object
+        self.id = str(data['supplier_id']).strip()                      # To clean spaces in Supplier ID
+        self.name = data['supplier_name'].strip()                       # To clean spaces in Supplier name
+        self.contact = data['contact_person'].strip()                   # To clean spaces in contact person
+        self.email = data['email'].strip()                              # To clean spaces in email address
+        self.phone = data['phone'].strip()                              # To clean spaces in phone number
 
-    def __init__(self):                 # Create a new SupplierDirectory object
-        self.suppliers = {}             # Create an empty container to store information about the suppliers
+class SupplierDirectory:                                                # Define a class called SupplierDirectory to manage multiple suppliers
+    def __init__(self):                                                 # Create a constructor methnod and create SupplierDirectory object
+        self.suppliers = {}                                             # Create an empty container to store information about the supplier objects
     
-    def add_supplier(self, supplier_name, contact, email, phone):       # Define a method called add_supplier that stores supplier details to self.supplier dictionary.
-        self.suppliers[supplier_name] = {
-            "contact": contact,
-            "email": email,
-            "phone": phone
+    def add_supplier(self, supplier_name, contact, email, phone):       # Define a method called add_supplier that stores new supplier details to self.supplier dictionary.
+        self.suppliers[supplier_name] = {                               # Use supplier_name as a dictionary key an store contact, email and phone details in the same dictonary
+            "contact_person": contact.strip(),                          # Save the contact 
+            "email": email.strip(),
+            "phone": phone.strip()
         }
         
-    def get_supplier(self, supplier_name):  # Define a method called get_supplier to check the supplier's name.
-        return self.suppliers.get(supplier_name, {})        # if the supplier exists, return the details of the supplier. If it cannot be found, return the empty dictionary for later use.
+    def get_supplier(self, supplier_name):                              # Define a method called get_supplier to check the supplier's name.
+        return self.suppliers.get(supplier_name, {})                    # if the supplier exists, return the details of the supplier. If it cannot be found, return the empty dictionary for later use.
 
 
 
